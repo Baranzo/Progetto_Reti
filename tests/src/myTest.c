007f84ccbfa48a7ff246f8b2b42d10743b64b36e
baranzo <iz4ykn@gmail.com>
2014-10-28 16:49:06 +0100

    pulizia e organizzazione

--- tests/src/myTest.c
+++ tests/src/myTest.c
@@ -1,7 +1,7 @@
 /*
  * myTest.c
  * 
- * Copyright 2014 baranzo <baranzo@Yuna>
+ * Copyright 2014 thegamer <thegamer@littlewhite> baranzo <baranzo@Yuna>
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,56 +30,7 @@
  * veramente poche quindi devo rivedere un po' di cose
 */
  
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <string.h>
-#include <assert.h>
-
-
-#include <jansson.h>
-
-
-/* allora qui ci va una premessa, le strutture dati le ho buttate giù 
- * senza pensarci più di troppo e non sono sicuro sia il modo migliore di
- * impostare il lavoro, intanto faccio i test così poi vediamo. 
- * Sicuramente bisognerà fare degli enum per controllare che tutta la 
- * baracca vada per dove deve andare
-*/ 
- 
-
-typedef struct ipAddr {
-	
-	char version[30];
-	char addr[30];
-	char scope[20];
-	struct ipAddr *next;
-	
-}ipAddr;
-
-typedef struct interface {
-	
-	char type[20];
-	char ker_name[10];
-	char mac_addr[20];
-	char dev[30];
-	struct interface *next;
-	ipAddr *ip;
-	
-}interface;
-
-typedef struct sys_info{
-	
-	char hostname[20];
-	char os_name [20];
-	char os_version [10];
-	char manufacturer[30];
-	int memory;
-	interface *interfaces;
-}sys_info;
-
-
+#include "myTest.h"
 
 //dummy del piffero, intanto provo ste benedette funzioni
 sys_info* fill_dummy(sys_info *pt)
---
+++ tests/src/myTest.h
@@ -0,0 +1,49 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <assert.h>
+
+
+#include <jansson.h>
+
+
+/* allora qui ci va una premessa, le strutture dati le ho buttate giù 
+ * senza pensarci più di troppo e non sono sicuro sia il modo migliore di
+ * impostare il lavoro, intanto faccio i test così poi vediamo. 
+ * Sicuramente bisognerà fare degli enum per controllare che tutta la 
+ * baracca vada per dove deve andare
+*/ 
+ 
+
+typedef struct ipAddr {
+	
+	char version[30];
+	char addr[30];
+	char scope[20];
+	struct ipAddr *next;
+	
+}ipAddr;
+
+typedef struct interface {
+	
+	char type[20];
+	char ker_name[10];
+	char mac_addr[20];
+	char dev[30];
+	struct interface *next;
+	ipAddr *ip;
+	
+}interface;
+
+typedef struct sys_info{
+	
+	char hostname[20];
+	char os_name [20];
+	char os_version [10];
+	char manufacturer[30];
+	int memory;
+	interface *interfaces;
+}sys_info;
+
